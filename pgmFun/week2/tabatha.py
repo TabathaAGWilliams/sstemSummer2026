68a2ae4cf78a4bbfda923aab1654306288ab3d6b
TabathaAGWilliams <geltta@appstate.edu>
2026-06-05 11:32:01 -0400

    lots of basic things!

--- pgmFun/week2/tabatha.py
+++ pgmFun/week2/tabatha.py
@@ -3,6 +3,24 @@ image = [
     [[0, 0, 255], [255, 255, 255]]
 ]
 
+def read_ppm(filename):
+    with open(filename, "r") as file:
+        data = file.read().split()
+        magic_num = data[0]
+        w = int(data[1])
+        h = int(data[2])
+        max_value = int(data[3])
+        pixels = [int(x) for x in data[4:]]
+        
+        rgb_pixels = []
+        for i in range(0, len(pixels), 3):
+            rgb_pixels.append([
+                pixels[i],
+                pixels[i+1],
+                pixels[i+2]
+            ])
+    return rgb_pixels
+
 height = len(image)
 width = len(image[0])
 
@@ -15,3 +33,8 @@ with open("output.ppm", "w") as f:
         for c in row:
             r, g, b = c
             f.write(f"{r} {g} {b}\n")
+
+img = read_ppm("practice.ppm")
+print(img[0])
+
+
